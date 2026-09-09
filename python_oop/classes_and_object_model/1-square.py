f43ba52f9469f3ff8983d1895d3caee576e9c132
lau-ro19 <lroseantoinette@gmail.com>
2026-09-09 12:07:50 +0200

    Add private instance attribute size to Square class

---
+++ python_oop/classes_and_object_model/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Module that defines a Square class with a private size attribute."""
+
+
+class Square:
+    """Represent a square."""
+
+    def __init__(self, size):
+        """Initialize a new Square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        self.__size = size
