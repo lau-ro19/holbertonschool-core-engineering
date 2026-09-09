e577cbd7fbe020b0c0091296ded923088f488ac4
lau-ro19 <lroseantoinette@gmail.com>
2026-09-09 13:17:00 +0200

    Fix shebang formatting in 2-rectangle.py

---
+++ python_oop/inheritance/2-rectangle.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""Module that defines a Rectangle class with area and string representation."""
+BaseGeometry = __import__('base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Represent a rectangle using BaseGeometry."""
+
+    def __init__(self, width, height):
+        """Initialize a new Rectangle.
+
+        Args:
+            width (int): The width of the rectangle.
+            height (int): The height of the rectangle.
+        """
+        self.integer_validator("width", width)
+        self.__width = width
+
+        self.integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        """Calculate and return the area of the rectangle."""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """Return the string representation of the rectangle."""
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
